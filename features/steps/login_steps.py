6080ecd9295a8bca20433be12077a4276fabb280
Aswathi-QA <mailtoaswathicheruvath@gmail.com>
2026-05-22 04:52:36 +0000

    modified login steps and requirement

--- features/steps/login_steps.py
+++ features/steps/login_steps.py
@@ -1,18 +1,36 @@
 from behave import given, when, then
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.firefox.options import Options
+avc
 @given('user is on login page')
 def step_impl(context):
-    service = Service(ChromeDriverManager().install())
-
     options = Options()
-    options.add_argument("--headless=new")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--headless")
 
-    context.driver = webdriver.Chrome(service=service, options=options)
+    # ✅ Selenium Manager handles geckodriver
+    context.driver = webdriver.Firefox(options=options)
     context.driver.get("https://the-internet.herokuapp.com/login")
+
+@when('user enters valid username and password')
+def step_impl(context):
+    context.driver.find_element(By.ID, "username").send_keys("tomsmith")
+    context.driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
+    context.driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+@then('user should see secure area')
+def step_impl(context):
+    assert "secure" in context.driver.current_url.lower()
+    context.driver.quit()
+
+@when('user enters invalid username and password')
+def step_impl(context):
+    context.driver.find_element(By.ID, "username").send_keys("wrong")
+    context.driver.find_element(By.ID, "password").send_keys("wrong")
+    context.driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+@then('user should see error message')
+def step_impl(context):
+    error = context.driver.find_element(By.ID, "flash").text
+    assert "invalid" in error.lower()
+    context.driver.quit()
