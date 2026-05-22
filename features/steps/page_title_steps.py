8da1b5c076cc4957d6a4ce7e413f6558ddc53c0e
Aswathi-QA <mailtoaswathicheruvath@gmail.com>
2026-05-22 07:25:22 +0000

    Fix ambiguous step by reusing existing login navigation step

--- features/steps/page_title_steps.py
+++ features/steps/page_title_steps.py
@@ -1,9 +1,5 @@
 from behave import given, then
 
-@given('user is on login page')
-def step_impl(context):
-    context.driver.get("https://the-internet.herokuapp.com/login")
-
 @then('the page title should be displayed')
 def step_impl(context):
     assert context.driver.title is not None
