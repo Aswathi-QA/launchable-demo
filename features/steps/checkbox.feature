3145dcb76eed8575db9af413e58a6d61cd595742
Aswathi-QA <mailtoaswathicheruvath@gmail.com>
2026-05-22 05:08:10 +0000

    added checkbox feature and steps

---
+++ features/steps/checkbox.feature
@@ -0,0 +1,6 @@
+Feature: Checkbox selection
+
+  Scenario: Select first checkbox
+    Given user is on checkbox page
+    When user selects checkbox one
+    Then checkbox one should be selected
---
+++ features/steps/checkbox_steps.py
@@ -0,0 +1,17 @@
+from behave import given, when, then
+from selenium.webdriver.common.by import By
+
+@given('user is on checkbox page')
+def step_impl(context):
+    context.driver.get("https://the-internet.herokuapp.com/checkboxes")
+
+@when('user selects checkbox one')
+def step_impl(context):
+    checkbox = context.driver.find_elements(By.CSS_SELECTOR, "input[type='checkbox']")[0]
+    checkbox.click()
+
+@then('checkbox one should be selected')
+def step_impl(context):
+    checkbox = context.driver.find_elements(By.CSS_SELECTOR, "input[type='checkbox']")[0]
+    assert checkbox.is_selected()
+    context.driver.quit()
