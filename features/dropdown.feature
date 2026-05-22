483247c69a08e28711c5830dcdc94a2ab3e88669
Aswathi-QA <mailtoaswathicheruvath@gmail.com>
2026-05-22 05:10:32 +0000

    added dropdown feature and steps

---
+++ features/dropdown.feature
@@ -0,0 +1,6 @@
+Feature: Dropdown selection
+
+  Scenario: Select option from dropdown
+    Given user is on dropdown page
+    When user selects option two
+    Then option two should be selected
---
+++ features/steps/dropdown_steps.py
@@ -0,0 +1,18 @@
+from behave import given, when, then
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+@given('user is on dropdown page')
+def step_impl(context):
+    context.driver.get("https://the-internet.herokuapp.com/dropdown")
+
+@when('user selects option two')
+def step_impl(context):
+    dropdown = Select(context.driver.find_element(By.ID, "dropdown"))
+    dropdown.select_by_visible_text("Option 2")
+
+@then('option two should be selected')
+def step_impl(context):
+    selected = context.driver.find_element(By.CSS_SELECTOR, "#dropdown option:checked").text
+    assert selected == "Option 2"
+    context.driver.quit()
