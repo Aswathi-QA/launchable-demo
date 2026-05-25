ba761a78f214e355e396ba661c164b0bf16784b3
Aswathi-QA <mailtoaswathicheruvath@gmail.com>
2026-05-25 09:26:01 +0000

    Update environment.py for Firefox headless support

--- features/environment.py
+++ features/environment.py
@@ -4,7 +4,17 @@ from selenium.webdriver.firefox.options import Options
 def before_scenario(context, scenario):
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    # Reduce Firefox internal logging (safe)
+    options.log.level = "fatal"
+
     context.driver = webdriver.Firefox(options=options)
 
 def after_scenario(context, scenario):
-    context.driver.quit()
+    if hasattr(context, "driver"):
+        try:
+            context.driver.quit()
+        except:
+            pass
